39f1b01830f9934016350f3270c678ca7ed67891
Joseph-plex <joseph@plexxis.com>
2014-05-13 06:17:44 -0400

    Initial Commit

---
+++ DragonNest.SkillSimulator/Job.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DragonNest.ResourceInspection.dnt;
+
+namespace DragonNest.SkillSimulator
+{
+    public struct Job
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public int JobId
+        {
+            get;
+            set;
+        }
+    }
+}
