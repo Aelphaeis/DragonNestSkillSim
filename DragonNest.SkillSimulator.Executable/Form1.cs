8b35ff995f174ec256810388e8a0f7ca84f6f75d
Joseph-plex <joseph@plexxis.com>
2014-05-13 07:11:27 -0400

    Change Log
    - Augmenting Job in Skill Simulation DLL. Saves us Trouble.
    - Minor Misc Changes.

--- DragonNest.SkillSimulator.Executable/Form1.cs
+++ DragonNest.SkillSimulator.Executable/Form1.cs
@@ -15,15 +15,31 @@ namespace DragonNest.SkillSimulator.Executable
     {
 
         DataTable SkillTableCharacter;
+        DataTable SkillTableCleric;
         public Form1()
         {
             InitializeComponent();
+            init();
         }
 
         void init()
         {
             using(var stream = new MemoryStream(DNTResources.skilltable_character))
-                SkillTableCharacter = new DragonNestDataTable();
+                SkillTableCharacter = new DragonNestDataTable(stream);
+
+            using (var stream = new MemoryStream(DNTResources.skillleveltable_charactercleric))
+                SkillTableCleric = new DragonNestDataTable(stream);
+
+            var viewable = SkillTableCharacter.Clone();
+            viewable.Rows.Clear();
+            
+            var rows = SkillTableCharacter.Rows.Cast<DataRow>();
+            foreach (var row in rows.Where(r => r["_NeedJob"].Equals(4)))
+                viewable.ImportRow(row);
+
+
+            dataGridView1.DataSource = viewable;
+
         }
 
     }
---
+++ DragonNest.SkillSimulator/Types/Job.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DragonNest.ResourceInspection.dnt;
+using System.Data;
+namespace DragonNest.SkillSimulator.Types
+{
+    using Dnt = DragonNestDataTable;
+
+    public class Job
+    {
+        const string RowIdColumnId = "_JobName";
+        const string ServiceColumnId = "_Service";
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public int JobId
+        {
+            get;
+            set;
+        }
+
+        public Job ()
+        {
+            Name = String.Empty;
+            JobId = new int();
+        }
+
+        public Job(int ClassId, Dnt JobTable, Dnt ClassTable)
+        {
+            //Check to make sure nothing is null
+            if (JobTable == null)
+                throw new ArgumentNullException("JobTable");
+            if (ClassTable == null)
+                throw new ArgumentNullException("ClassTable");
+
+            //Get Information pertaining to the Job.
+            var JobRow = JobTable.Rows.Cast<DataRow>().FirstOrDefault(p => p[RowIdColumnId].Equals(ClassId));
+
+            //If we cannot find that information throw exception
+            if (JobRow == null)
+                throw new Exception("Not Such Class Id");
+
+            //If we find the information but its not active, most likely the information pertaining to the skill tree is incomplete.
+            //Throw an error to prevent unexpected behaviour.
+            if (JobRow[ServiceColumnId].Equals(false))
+                throw new Exception("The Class you specified is not in Service");
+        }
+    }
+}
