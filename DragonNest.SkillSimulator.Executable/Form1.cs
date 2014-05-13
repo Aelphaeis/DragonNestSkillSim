a67756ca90be2849784281da8d3cd0d1ff0420f1
Joseph-plex <joseph@plexxis.com>
2014-05-13 06:35:41 -0400

    Change Log
    - added function to Load dnt tables into the executable.

--- DragonNest.SkillSimulator.Executable/Form1.cs
+++ DragonNest.SkillSimulator.Executable/Form1.cs
@@ -5,15 +5,26 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
-
+using DragonNest.ResourceInspection.dnt;
+using DragonNest.SkillSimulator.Executable;
 namespace DragonNest.SkillSimulator.Executable
 {
     public partial class Form1 : Form
     {
+
+        DataTable SkillTableCharacter;
         public Form1()
         {
             InitializeComponent();
         }
+
+        void init()
+        {
+            using(var stream = new MemoryStream(DNTResources.skilltable_character))
+                SkillTableCharacter = new DragonNestDataTable();
+        }
+
     }
 }
